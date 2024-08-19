2b875824d1bd95e8f036cfb9625a282a0b15b334
miguelangel708 <mimunoza@unal.edu.co>
2024-08-18 23:21:19 -0500

    add class attributes as api response and language switching

--- backend/src/models/validateActions.py
+++ backend/src/models/validateActions.py
@@ -1,52 +1,55 @@
 from dataclasses import dataclass
 import requests
-import os 
+import os
 from dotenv import load_dotenv
 import time
 
 
 class validateActions:
-    
-    def __init__(self, document_validation, country, document_type, user_authorized):        
+
+    validation_status: str
+    validation_id: str
+    processing_time: str
+    validation_date: str
+
+    def __init__(self, document_validation, country, document_type, user_authorized):
+        
         load_dotenv()
         self.TRUORA_APIKEY = os.environ.get("truoraApiKey", "")
         self.url = os.environ.get("truoraDocumentValidationURL", "")
-        
-        
+
         self.document_validation = document_validation
-        self.country= country
+        self.country = country
         self.document_type = document_type
         self.user_authorized = user_authorized
-        
-        
+
     def pipeline_document_validation(self):
-        # 1. conseguir la validación inicial y adquirir así el front_url y back_url
+        # 1. get the initial validation and thus acquire the front_url and back_url
         data = self.createValidation()
-        # 2. extraer los enlaces para hacer upload
+        # 2. extract links for uploading
         front_url = data["instructions"]["front_url"]
         reverse_url = data["instructions"]["reverse_url"]
         # upload del front
         validate_front_upload = self.uploadDocFrontPicture(front_url)
         # upload reverse
-        validate_reverse_upload  = self.uploadDocReversePicture(reverse_url)
+        validate_reverse_upload = self.uploadDocReversePicture(reverse_url)
         # validate the uploads
-        if not (validate_front_upload and validate_reverse_upload): return "error al cargar las images en el pipeline" , 400 
-        # extraer el id para la validación 
-        validation_id = data["validation_id"]
+        if not (validate_front_upload and validate_reverse_upload):
+            return "fail loading images in the pipeline", 400
+        # extract the id for validation
+        self.validation_id = data["validation_id"]
         # get validation
-        final_validation_status = self.check_validation_status(validation_id)
-        return final_validation_status
-        
-        
+        self.check_validation_status()
+
     def createValidation(self):
-        
+
         headers = {
             "Truora-API-Key": self.TRUORA_APIKEY,
             "Accept": "application/json",
             "Content-Type": "application/x-www-form-urlencoded"
         }
 
-        # Parámetros para filtrar datos en una solicitud GET
+        # Parameters for filtering data in a GET request
         params = {
             "type": "document-validation",
             "country": self.country,
@@ -58,92 +61,98 @@ class validateActions:
 
         if response.status_code == 200:
             data = response.json()
+            print("account id validation response: ", data["account_id"])
             print("validation created with id: ", data["validation_id"])
             return data
-        
         else:
             print(f"Error {response}")
 
-
     def uploadDocFrontPicture(self, front_url):
-                
+
         image_path = "uploads/front_doc.jpg"
-       
+
         headers = {
             "Truora-API-Key": self.TRUORA_APIKEY,
         }
-        
+
         with open(image_path, 'rb') as image_file:
-            response = requests.put(front_url, headers=headers, data=image_file)
+            response = requests.put(
+                front_url, headers=headers, data=image_file)
 
         if response.status_code == 200:
             data = response.json()
             print("nice upload front document picture")
             return True
-        
+
         else:
             print(f"Error {response.status_code}")
             return False
-        
 
     def uploadDocReversePicture(self, reverse_url):
-        
+
         image_path = "uploads/reverse_doc.jpg"
-        
+
         headers = {
             "Truora-API-Key": self.TRUORA_APIKEY,
         }
-        
+
         with open(image_path, 'rb') as image_file:
-            response = requests.put(reverse_url, headers=headers, data=image_file)
+            response = requests.put(
+                reverse_url, headers=headers, data=image_file)
 
         if response.status_code == 200:
             data = response.json()
             print("nice upload reverse document picture")
             return True
-        
+
         else:
             print(f"Error {response.status_code}")
             return False
-  
-        
+
     def getValidation(self, validation_id):
-        
+
         headers = {
             "Truora-API-Key": self.TRUORA_APIKEY
         }
-        
+
         url = self.url + "/" + validation_id
-        
+
         response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
-            
+
             data = response.json()
             validation_status = data["validation_status"]
+            validation_date = data["creation_date"]
             print("nice validation, status: ", validation_status)
-            return validation_status
-        
+            return validation_status, validation_date
+
         else:
             print(f"Error {response.status_code}")
             return False
-        
-    def check_validation_status(self, validation_id):
-        max_wait_time = 15  # Tiempo máximo de espera en segundos
-        elapsed_time = 0  # Tiempo transcurrido
-        
+
+    def check_validation_status(self):
+        max_wait_time = 30  # Maximum waiting time in seconds
+        elapsed_time = 0  # Time elapsed
+
         while elapsed_time < max_wait_time:
-            # Llamada a la función que consulta la API
-            validation_status = self.getValidation(validation_id)
-            
+            # Time elapsed
+            validation_status, validation_date = self.getValidation(
+                self.validation_id)
+
             if validation_status:
-                # Si el estado ya no es "pending", salir del bucle
+                # If the status is no longer “pending”, exit the loop.
                 if validation_status != "pending":
-                    return validation_status
-            
-            # Esperar 1 segundo antes de volver a consultar
+                    self.validation_status = validation_status
+                    self.processing_time = elapsed_time
+                    self.validation_date = validation_date
+                    return
+
+            # Wait 1 second before consulting again.
             time.sleep(1)
             elapsed_time += 1
-        
-        # Si se alcanza el tiempo máximo, devolver "pending"
-        return "pending"
+
+        # Wait 1 second before consulting again.
+        self.validation_status = "pending"
+        self.processing_time = elapsed_time
+        self.validation_date = validation_date
--- backend/src/routes/generalApi.py
+++ backend/src/routes/generalApi.py
@@ -1,77 +1,88 @@
 from flask import Blueprint, request, redirect, url_for, flash, current_app, jsonify
 import os
 from ..models.validateActions import  validateActions
+from datetime import datetime
 
 main = Blueprint("general_api", __name__)
 
 
-UPLOAD_FOLDER = 'uploads/'  # Ruta donde se guardarán los archivos
+UPLOAD_FOLDER = 'uploads/'  # Path where the files will be saved
 MAX_FILE_SIZE = 30 * 1024 * 1024  # 5 MB
-ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'png'} # Extensiones permitidas
+ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'png'} # Permitted extensions
 DOCUMENT_VALIDATION = False
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
-def allowed_file(filename):
+def allowedFile(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def validateFiles(file_front, file_back):    
-    #  Validación del tamaño del archivo en el backend
+    #  File size validation in the backend
     if len(file_front.read()) > MAX_FILE_SIZE:
         file_front.seek(0)
-        return "El archivo frontal es demasiado grande. El tamaño máximo permitido es 5 MB.", 400
+        return "The front file is too large. The maximum size allowed is 5 MB.", 400
     file_front.seek(0)
     if len(file_back.read()) > MAX_FILE_SIZE:
         file_back.seek(0)
-        return "El archivo frontal es demasiado grande. El tamaño máximo permitido es 5 MB.", 400
+        return "The front file is too large. The maximum size allowed is 5 MB.", 400
     file_back.seek(0)
 
     # Validación del tipo de archivo en el backend
-    if not allowed_file(file_front.filename):
-        return "Tipo de archivo frontal no permitido. Solo se permiten JPEG, JPG y PNG.", 400
-    if not allowed_file(file_back.filename):
-        return "Tipo de archivo trasero no permitido. Solo se permiten JPEG, JPG y PNG.", 400
+    if not allowedFile(file_front.filename):
+        return "Front file type not allowed. Only JPEG, JPG and PNG are allowed.", 400
+    if not allowedFile(file_back.filename):
+        return "Front file type not allowed. Only JPEG, JPG and PNG are allowed.", 400
     
     DOCUMENT_VALIDATION = True
 
 
 @main.route('/upload', methods=['POST'])
-def upload_file():
+def uploadFile():
     
-    neededData = ['fileFront','fileBack','country','docType','acceptTerms']
-    
-    # validar que se hayan enviado todos los datos
+    # validate that all data have been sent
     try:
-        # Obtener los archivos del formulario
+        # Obtain form files
         file_front = request.files.get('fileFront')
         file_back = request.files.get('fileBack')
-        # Obtener otros datos del formulario
+        # Obtain other data from the form
         country = request.form.get('country')
         doc_type = request.form.get('docType')
-        accept_terms = request.form.get('acceptTerms') == 'true'  # Convertir a booleano
+        accept_terms = request.form.get('acceptTerms') == 'true'  # convert to boolean
     except Exception as e:
-        return "no se encuentran todos los datos en el formulario o error al asignarlos", 400
+        return "not all data are found in the form or error when assigning data", 400
 
-    # validar que las imagenes cumplan los requisitos
+    # validate that the images meet the requirements
     validateFiles(file_front, file_back)
 
-    # Imprimir todos los datos recibidos
-    print(f'País: {country}')
-    print(f'Tipo de documento: {doc_type}')
-    print(f'Términos aceptados: {accept_terms}')
-    
-    # Guardar las imágenes en la carpeta 'updates'
+    # Save the images in the 'updates' folder
     if file_front:
         file_front.save(os.path.join(UPLOAD_FOLDER, "front_doc.jpg"))
-        print(f'Imagen de frente guardada')
+        print(f'Saved front image')
     
     if file_back:
         file_back.save(os.path.join(UPLOAD_FOLDER, "reverse_doc.jpg"))
-        print(f'Imagen de respaldo guardad')
+        print(f'Backup image saved')
     
     validate_actions = validateActions(DOCUMENT_VALIDATION, country, doc_type, accept_terms)
-    final_validation_status = validate_actions.pipeline_document_validation()
+    validate_actions.pipeline_document_validation()
     
-    return jsonify({'status': final_validation_status}), 200
+
+    # Suppose that validation_date is the date in UTC format that you want to convert
+    validation_date = validate_actions.validation_date
+    # Truncate the microseconds part to 6 digits
+    validation_date_truncated = validation_date[:26] + "Z"
+    # Truncate the microseconds part to 6 digits
+    validation_formatted_date = datetime.strptime(validation_date_truncated, "%Y-%m-%dT%H:%M:%S.%fZ")
+    # Format the date and time in the format DD/MM/YYYYYY HH:MM:SS
+    validation_formatted_date = validation_formatted_date.strftime("%d/%m/%Y %H:%M:%S")
+    
+    
+    response = {
+        'status':validate_actions.validation_status,
+        'validation_date':validation_formatted_date,
+        'processing_time':validate_actions.processing_time
+    }
+    
+    return jsonify(response), 200
